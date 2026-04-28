c8af607c6a1de49444a227b281fab7f69b19240b
hoang <83692822+hoang227@users.noreply.github.com>
2026-04-28 15:52:44 +0800

    Add: find-pivot-index - submission-1

---
+++ Data Structures & Algorithms/find-pivot-index/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    pivotIndex(nums) {
+        const sum = nums.reduce((curr, acc) => acc += curr, 0)
+        let forward = 0, back = sum - nums[0], ptr = 0
+        
+        while(ptr < nums.length) {
+            if (forward === back) return ptr
+            forward += nums[ptr]
+            back -= nums[ptr + 1]
+            console.log(forward, back, ptr)
+
+            ptr++
+        }
+
+        return -1
+    }
+}
